4b213f0ef891b42e5ac2de01f774e7f9f6362623
John F. MacDonald <john@jfmacdonald.com>
2022-07-28 04:58:36 -0700

    set up app with empty sandbox component

---
+++ src/app/sandbox/sandbox.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sandbox',
+  template: `
+    <p>
+      sandbox works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SandboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
